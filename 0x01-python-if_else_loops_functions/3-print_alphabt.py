51f1fb2f98e7c2a64ff782f1645caa1155a277be
Alvin05-maker <alvinoagaromba22@gmsil.com>
2023-08-08 14:24:54 -0700

    pycode style validation

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for alphabets in range(97,123):
+for alphabets in range(97, 123):
     if alphabets == 101 or alphabets == 113:
         continue
     print("{}".format(chr(alphabets)), end="")
