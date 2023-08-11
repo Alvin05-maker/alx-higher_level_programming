777048dccf228fd21953c2161a12ea43e25415ac
Alvin05-maker <alvinoagaromba22@gmsil.com>
2023-08-11 07:32:10 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
             print("{} argument:".format(args))
         else:
             print("{} arguments:".format(args))
-    for i in range(args):
-        print("{} : {}".format(i + 1, sys.argv[i + 1])) 
+for i in range(args):
+    print("{} : {}".format(i + 1, sys.argv[i + 1])) 
