c718679bd08750d2bdd3736ca2dfedb239710647
Alvin05-maker <alvinoagaromba22@gmsil.com>
2023-08-14 16:04:48 -0700

    update header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -7,5 +7,6 @@ def print_reversed_list_integer(my_list=[]);
 def new_in_list(my_list, idx, element);
 def print_matrix_integer(matrix=[[]]);
 def delete_at(my_list=[], idx=0);
-def divisible_by_2(my_list=[])
+def divisible_by_2(my_list=[]);;
+
 #endif /* LISTS_H*
