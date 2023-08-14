184e4050775efaa047ce76b46af9858b2911fa9b
Alvin05-maker <alvinoagaromba22@gmsil.com>
2023-08-14 15:36:31 -0700

    list operations

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -5,4 +5,7 @@ def print_list_integer(my_list=[]);
 def element_at(my_list, idx);
 def print_reversed_list_integer(my_list=[]);
 def new_in_list(my_list, idx, element);
+def print_matrix_integer(matrix=[[]]);
+def delete_at(my_list=[], idx=0);
+
 #endif /* LISTS_H*
