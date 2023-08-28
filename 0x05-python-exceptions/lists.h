c834cfbec0a143518d347c34c2f3c44d0d81d86b
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-08-28 16:44:31 -0700

    updated header and raise exception files

--- 0x05-python-exceptions/lists.h
+++ 0x05-python-exceptions/lists.h
@@ -2,6 +2,12 @@
 #define LISTS_H
 
 def safe_print_list(my_list=[], x=0);
-
-
+def safe_print_list_integers(my_list=[], x=0);;
+def safe_print_integer(value);
+def safe_print_division(a, b);
+def list_division(my_list_1, my_list_2, list_length);
+def raise_exception();
+def raise_exception_msg(message="");
+def magic_calculation(a, b);
+def safe_print_integer_err(value);
 #endif /* LISTS */
