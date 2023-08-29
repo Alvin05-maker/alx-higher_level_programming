335fd32def37ec23321cd83c46553e3439486209
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-08-29 16:39:54 -0700

    print area of a square using #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Define a cass Square"""
+
+
+class Square:
+    """Representation of the class Square"""
+
+    def __init__(self, size=0):
+        """Initiaization of the class.
+
+        Args:
+            size(int): size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of the square"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
