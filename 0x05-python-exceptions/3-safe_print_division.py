deb49a23a168da1af710f158c99e6dd8bd509350
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-08-30 09:50:05 -0700

    print integer

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""Function to divide two integers and print the result"""
+
 
 def safe_print_division(a, b):
+    """Define the function .
+
+    Args:
+        a(int): The integer to divide .
+        b(int): The divisor .
+    """
     try:
         division = a/b
     except (TypeError, ZeroDivisionError):
