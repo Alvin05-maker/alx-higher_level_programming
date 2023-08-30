c0957a6329b2480527d1cc75ebe395417736d569
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-08-30 12:56:23 -0700

    print integer and err in stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Print an integer and error to stderr"""
-
+import sys
 
 def safe_print_integer_err(value):
     """print an integer .
@@ -12,5 +12,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return (True)
     except Exception as e:
-        print("Exception:", e)
+        print("Exception: {}".format(e), file=sys.stderr)
         return (False)
