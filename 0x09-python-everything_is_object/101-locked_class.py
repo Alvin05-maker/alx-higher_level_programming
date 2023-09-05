bb5ae54b198cce0fb987f60c46cb647477b2ad07
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-09-05 14:50:37 -0700

    locked class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class LockedClass:
+    def __setattr__(self, name, value):
+        if name == 'first_name':
+            # Allow creating the 'first_name' attribute
+            self.__dict__[name] = value
+        else:
+            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
+
