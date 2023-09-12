72ebef18a33c322e280e3bf23af604a9153b1508
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-09-12 16:17:00 -0700

    Return all available attributes and methods in an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Define a function thatinds attributes and methods of a given object."""
+
 
 def lookup(obj):
     """ Returns all available attributes and methods of an object."""
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,6 +2,9 @@
 
 class MyList(list):
     """ MyList class that inherits from the builtin list. """
+    def __init__(self, iterable):
+        """initializes MyList class"""
+        super().__init__([int(item) for item in iterable])
     def print_sorted(self):
         """ Prints the list inn ascending sort order. """
         sorted_list = sorted(self)
