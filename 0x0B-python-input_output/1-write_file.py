ab6a3b1c455af841ee71c50a870083e92a99657a
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-09-12 12:17:48 -0700

    write text to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Define a function that writes text to a file."""
+
+def write_file(filename="", text=""):
+    """Writes a text to a file.
+
+    Args:
+        filename(str): name of file to write to.
+        text(str): text to write.
+
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
