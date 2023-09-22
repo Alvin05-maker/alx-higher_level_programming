42865a4dd8024644b82a078c4862efb0aefc0e38
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-09-22 22:48:02 +0000

    update class rectangle and square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -34,25 +34,24 @@ class Square(Rectangle):
 		**kwargs (dict): New key/value pairs of attributes.
 		"""
 		if args and len(args) != 0:
-		a = 0
-		for arg in args:
-                	if a == 0:
-                    		if arg is None:
-					self.__init__(self.size, self.x, self.y)
-				else:
-                        		self.id = arg
-			elif a == 1:
-				self.size = arg
-			elif a == 2:
-				self.x = arg
-			elif a == 3:
-				self.y = arg
-			a += 1
-		
+			a = 0
+			for arg in args:
+				if a == 0:
+					if arg is None:
+						self.__init__(self.size, self.x, self.y)
+					else:
+                        			self.id = arg
+				elif a == 1:
+					self.size = arg
+				elif a == 2:
+					self.x = arg
+				elif a == 3:
+					self.y = arg
+				a += 1
 		elif kwargs and len(kwargs) != 0:
 			for k, v in kwargs.items():
 				if k == "id":
- 					if v is None:
+					if v is None:
 						self.__init__(self.size, self.x, self.y)
 					else:
 						self.id = v
@@ -62,16 +61,15 @@ class Square(Rectangle):
 					self.x = v
 				elif k == "y":
 					self.y = v
-    def to_dictionary(self):
-        """Return the dictionary representation of the Square."""
-        return {
-            "id": self.id,
-            "size": self.width,
-            "x": self.x,
-            "y": self.y
-        }
-
-    def __str__(self):
-        """Return the print() and str() representation of a Square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+	def to_dictionary(self):
+		"""Return the dictionary representation of the Square."""
+		return {
+			"id": self.id,
+			"size": self.width,
+			"x": self.x,
+			"y": self.y
+			}
+	def __str__(self):
+		"""Return the print() and str() representation of a Square."""
+		return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
