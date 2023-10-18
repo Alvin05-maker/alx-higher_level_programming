831e4655600f1eac5676aecbe59bbef4ac4ddbdf
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-10-18 15:00:53 -0700

    create read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- create a user and a database and grant the user SELECT privilege only;
+CREATE USER  IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF  NOT EXISTS hbtn_0d_2;;
+GRANT SELECT ON hbtn_0d_2.* TO  'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
