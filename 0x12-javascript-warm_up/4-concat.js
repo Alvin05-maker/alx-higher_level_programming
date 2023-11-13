7609defc3551dd9a2502d46dce5b705f634b04c8
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-11-13 11:58:06 -0800

    Concatenate strings

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let concatString = '';
+if (process.argv.length  <= 2) {
+	process.argv[2] = 'undefined';
+	process.argv[3] = 'undefined';
+}
+else {
+	concatString += process.argv[2] + " " + "is" + " " + process.argv[3];
+}
+console.log(concatString);
