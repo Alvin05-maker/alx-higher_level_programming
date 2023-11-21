c81d65d71d6c55278b9d811c089dc3109cf911cb
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-11-21 10:44:27 -0800

    model state updated

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,7 +9,9 @@ custom_metadata = MetaData()
 Base = declarative_base(metadata=custom_metadata)
 
 class State(Base):
-    """Class definition of the model of the State class."""
+    """
+    Class definition f the model of the State class.
+    """
     __tablename__= 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
