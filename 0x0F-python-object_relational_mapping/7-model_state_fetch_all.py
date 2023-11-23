efbf61e5035b0b1d7fb7b2e4c6854594e417b64c
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-11-23 10:53:38 -0800

    featc all from the state class

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,12 +4,12 @@ List state objects from a database
 """
 
 from model_state import Base, State
-from sqlalchemy import create engine
-from sqlalchmey.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 import sys
 
 
-if __name__ = 'main':
+if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
 
