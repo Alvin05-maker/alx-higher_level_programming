2112a14302cac2b5bf9c0ccd8c271c411bad95ef
Alvin05-maker <alvinoagaromba22@gmail.com>
2024-02-09 05:46:22 -0800

    multi languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
-<<<<<<< HEAD
-let i = 0;
-const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-while (i < lang.length) {
-  console.log(lang[i]);
-  i++;
-=======
-const myvar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i;
-for (i = 0; i < myvar.length; i++) {
-  console.log(myvar[i]);
->>>>>>> 442334e2b4cd12fa360262b74d95815765befe45
-}
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
