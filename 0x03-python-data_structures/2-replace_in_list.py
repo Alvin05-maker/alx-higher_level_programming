4c02c18b05cdf016f84608ea8c04cdbbeb776b2c
Alvin05-maker <alvinoagaromba22@gmail.com>
2024-02-21 23:36:14 -0800

    replace an element at an index

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 #replace element
 def replace_in_list(my_list, idx, element):
+    """Replaces an element in list
+
+    Args:
+    my_list: The list to replace an  elemnet in
+    idx: The index to replace an element at
+    element: The element to replace with
+
+    Returns:
+    The new list with the replaced element
+    """
     if idx > 0 or idx <= len(my_list):
         my_list[idx] = element
         return my_list
